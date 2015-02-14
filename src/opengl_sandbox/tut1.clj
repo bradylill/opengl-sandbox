dbd1552eca0d8a35cccd6d8d49fa04d0e9883b30
brady <bradylill@gmail.com>
2015-02-14 20:43:06 +0000

    Tutorial 1 - a basic window.

--- src/opengl_sandbox/tut1.clj
+++ src/opengl_sandbox/tut1.clj
@@ -1,5 +1,13 @@
 (ns opengl-sandbox.tut1
    (:import [org.lwjgl.opengl DisplayMode Display]))
 
+(defn game-loop []
+  (while (not (Display/isCloseRequested))
+   (Display/update)))
+
 (defn run []
-  (println "Running tutorial 1"))
+  (println "Running tutorial 1")
+  (Display/setDisplayMode (DisplayMode. 800 600))
+  (Display/setTitle "Tutorial 1")
+  (Display/create)
+  (game-loop))

7f63006f3d3286a317fc13ab110366196dc6790c
brady <bradylill@gmail.com>
2015-02-14 21:23:35 +0000

    Tutorial 3 - Drawing an old style OpenGL 1.1 orthographic quad.

--- src/opengl_sandbox/core.clj
+++ src/opengl_sandbox/core.clj
@@ -1,10 +1,11 @@
 (ns opengl-sandbox.core
   (:require [opengl-sandbox.tut1 :as tut1]
             [opengl-sandbox.tut2 :as tut2]
-            ))
+            [opengl-sandbox.tut3 :as tut3]))
 
 (def tutorials {:tut1 tut1/run
-                :tut2 tut2/run})
+                :tut2 tut2/run
+                :tut3 tut3/run})
 
 (defn -main [& args]
    (println "Starting")
---
+++ src/opengl_sandbox/tut3.clj
@@ -0,0 +1,37 @@
+(ns opengl-sandbox.tut3
+   (:import [org.lwjgl.opengl DisplayMode Display GL11]))
+
+(def screen-width 800)
+(def screen-height 600)
+
+(defn draw-quad []
+  (GL11/glClear (bit-or (GL11/GL_COLOR_BUFFER_BIT) (GL11/GL_DEPTH_BUFFER_BIT)))
+  (GL11/glColor3f 0.5 0.5 1.0)
+  (GL11/glBegin (GL11/GL_QUADS))
+  (GL11/glVertex2f 100 100)
+  (GL11/glVertex2f 300 100)
+  (GL11/glVertex2f 300 300)
+  (GL11/glVertex2f 100 300)
+  (GL11/glEnd))
+
+(defn game-loop []
+  (while (not (Display/isCloseRequested))
+    (draw-quad)
+    (Display/update)))
+
+(defn init-opengl []
+  (GL11/glMatrixMode (GL11/GL_PROJECTION))
+  (GL11/glLoadIdentity)
+  (GL11/glOrtho 0 screen-width 0 screen-height 1 -1)
+  (GL11/glMatrixMode (GL11/GL_MODELVIEW)))
+
+(defn create-display []
+  (Display/setDisplayMode (DisplayMode. screen-width screen-height))
+  (Display/setTitle "Tutorial 3")
+  (Display/create))
+
+(defn run []
+  (println "Running tutorial 3")
+  (create-display)
+  (init-opengl)
+  (game-loop))

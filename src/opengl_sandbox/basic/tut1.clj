859f38d3b5228210a12954ab3520c271d2f2b6de
brady <bradylill@gmail.com>
2015-02-15 15:03:44 +0000

    Forcing openGL to use core profile.

--- src/opengl_sandbox/basic/tut1.clj
+++ src/opengl_sandbox/basic/tut1.clj
@@ -1,5 +1,5 @@
 (ns opengl-sandbox.basic.tut1
-   (:import [org.lwjgl.opengl DisplayMode Display]))
+   (:import [org.lwjgl.opengl DisplayMode Display PixelFormat ContextAttribs GL11]))
 
 (defn game-loop []
   (while (not (Display/isCloseRequested))
@@ -9,5 +9,9 @@
   (println "Running tutorial 1")
   (Display/setDisplayMode (DisplayMode. 800 600))
   (Display/setTitle "Tutorial 1")
-  (Display/create)
+  (Display/create (PixelFormat.) (-> (ContextAttribs. 3 2)
+                                     (.withForwardCompatible true)
+                                     (.withProfileCore true)))
+  (println "OpenGL version: "  (GL11/glGetString GL11/GL_VERSION))
+  (println "OpenGL renderer: " (GL11/glGetString GL11/GL_RENDERER))
   (game-loop))
